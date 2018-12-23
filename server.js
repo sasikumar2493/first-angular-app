3c024875cf340f59759abafd638617f70dbad670
sasikumar2493 <sasikumar2493@gmail.com>
2018-12-23 15:23:36 +0530

    Second Heroku commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/first-angular-app'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/first-angular-app/index.html'));
